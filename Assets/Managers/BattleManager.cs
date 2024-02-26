88154f261b8b1e66f5ef5c9c9815d129a2ccf022
YukiMabuchi <yuk1mabuchi1999@gmail.com>
2024-02-27 01:29:28 +0900

    Add Menu button and popup

--- Assets/Managers/BattleManager.cs
+++ Assets/Managers/BattleManager.cs
@@ -4,10 +4,12 @@ using UnityEngine.SceneManagement;
 public class BattleManager : MonoBehaviour
 {
     [SerializeField] GameObject gameOverPopup;
+    [SerializeField] GameObject menuPopup;
 
     private void Awake()
     {
         gameOverPopup.SetActive(false);
+        menuPopup.SetActive(false);
     }
 
     public void GameOver()
@@ -36,5 +38,17 @@ public class BattleManager : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    public void OpenMenu()
+    {
+        Time.timeScale = 0;
+        menuPopup.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        ResumeGame();
+        menuPopup.SetActive(false);
     }
 }
